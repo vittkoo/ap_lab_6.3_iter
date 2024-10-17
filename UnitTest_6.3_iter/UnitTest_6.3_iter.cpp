8b8ae69d13f975c56e891b96f25ea3830f1b5e33
vittkoo <dmytrovych.vv@gmail.com>
2024-10-17 14:58:43 +0300

    commit_1

---
+++ UnitTest_6.3_iter/UnitTest_6.3_iter.cpp
@@ -0,0 +1,53 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../ap_lab_6.3_iter/lab_6.3_iter.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestForArray
+{
+	TEST_CLASS(UnitTestForArray)
+	{
+	public:
+
+		TEST_METHOD(TestCreateArray)
+		{
+			const int n = 5;
+			int b[n];
+			int Low = -10, High = 10;
+
+			Create(b, n, Low, High);
+
+			for (int i = 0; i < n; i++) {
+				Assert::IsTrue(b[i] >= Low && b[i] <= High);
+			}
+		}
+
+		TEST_METHOD(TestMinOdd)
+		{
+			// 1. масив з непарними елементами
+			const int n1 = 5;
+			int a1[n1] = { 12, 7, 5, 20, 9 };
+			int expectedMinOdd1 = 5;
+
+			int actualMinOdd1 = MinOdd(a1, n1);
+			Assert::AreEqual(expectedMinOdd1, actualMinOdd1);
+
+			// 2. масив без непарних елементів
+			const int n2 = 4;
+			int a2[n2] = { 12, 22, 28, 46 };
+			int expectedMinOdd2 = 0;
+
+			int actualMinOdd2 = MinOdd(a2, n2);
+			Assert::AreEqual(expectedMinOdd2, actualMinOdd2);
+
+			// 3. масив з одним непарним елементом
+			const int n3 = 4;
+			int a3[n3] = { 12, 14, 9, 22 };
+			int expectedMinOdd3 = 9;
+
+			int actualMinOdd3 = MinOdd(a3, n3);
+			Assert::AreEqual(expectedMinOdd3, actualMinOdd3);
+		}
+	};
+}
---
+++ ap_lab_6.3_iter/lab_6.3_iter.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+
+void Create(int* a, const int size, const int Low, const int High) {
+    for (int i = 0; i < size; i++)
+        a[i] = Low + rand() % (High - Low + 1);
+}
+
+void Print(int* a, const int size) {
+    for (int i = 0; i < size; i++)
+        cout << "a[" << setw(2) << i << " ] = " << setw(4) << a[i] << endl;
+    cout << endl;
+}
+
+int MinOdd(int* a, const int size) {
+    int minOdd;
+    int imin = -1;
+
+    // пошук першого непарного елемента
+    for (int i = 0; i < size; i++) {
+        if (a[i] % 2 != 0) {
+            minOdd = a[i];
+            imin = i;
+            break;
+        }
+    }
+
+    if (imin == -1) {
+        cerr << "There`s no odd elements" << endl;
+        return 0;
+    }
+
+    // Пошук мінімального серед непарних
+    for (int i = imin + 1; i < size; i++) {
+        if (a[i] % 2 != 0 && a[i] < minOdd) {
+            minOdd = a[i];
+        }
+    }
+
+    return minOdd;
+}
+
+int main() {
+    srand((unsigned)time(NULL));
+
+    const int n = 10;
+    int a[n];
+
+    int Low = -10;
+    int High = 10;
+
+    Create(a, n, Low, High);
+    Print(a, n);
+
+    cout << "minOdd = " << MinOdd(a, n) << endl;
+
+    return 0;
+}
